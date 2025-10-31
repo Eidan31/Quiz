63d8cffb4b121c8bb31bdef7f1bf783b4828cd31
Laboratorio Estudiantes <laboratorio@ulatina.net>
2025-10-30 19:41:58 -0600

    La carpeta del pograma

---
+++ Calculadora 3/Calculadora 3/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Calculadora_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Num1 = 0;
+            double Num2 = 0;
+            double Resultado = 0;
+            String operacion = "";
+
+            Console.Write("ingrese el primer numero: ");
+            Num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("ingrese el segundo numero: ");
+            Num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la operacion (+, -, *, /)");
+            operacion = Console.ReadLine();
+
+            switch (operacion)
+            {
+                case "+":
+                    Resultado = Num1 + Num2;
+                    break;
+                case "-":
+                    Resultado = Num1 - Num2;
+                    break;
+                case "*":
+                    Resultado = Num1 * Num2;
+                    break;
+                case "/":
+                    if (Num2 != 0)
+                    {
+                        Resultado = Num1 / Num2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: No se puede dividir por cero.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Error opcion no valida.");
+                    return;
+            }
+
+            Console.WriteLine($"\nEl resultado de {Num1} {operacion} {Num2} es: {Resultado}");
+
+            Console.WriteLine("\nPresione cualquier tecla para finalizar...");
+            Console.ReadKey();
+        }
+    }
+}
